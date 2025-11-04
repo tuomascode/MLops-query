c011c722a76cbcbf7eb5585c1f7137bfb6708169
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2025-11-04 15:24:24 +0200

    finished product

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import uvicorn
 import httpx
 
 from src.models import APIResponse
+import time
+import http
 
 app = FastAPI()
 
@@ -18,18 +20,20 @@ def check_length(fact: str, expected_length: int) -> bool:
 @app.get("/")
 async def main():
     async with httpx.AsyncClient() as client:
+        start = time.monotonic()
         resp = await client.get("https://catfact.ninja/fact")
-        resp.raise_for_status()
+        latency = time.monotonic() - start
 
         resp_json = resp.json()
         fact = resp_json['fact']
         length = resp_json['length']
+
         return APIResponse(
-            latency=0,
-            failed_request=True,
+            latency=latency,
+            failed_request=resp.status_code != http.HTTPStatus.OK,
             length_correct=check_length(fact, length),
             punctuation=check_punctuation(fact),
-        ).model_dump_json()
+        ).model_dump()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
