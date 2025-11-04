7cd3228fe804ee9413e5e083567cd1482d77b74c
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2025-11-04 15:18:27 +0200

    add skeleton for api

--- src/main.py
+++ src/main.py
@@ -2,15 +2,30 @@ from fastapi import FastAPI
 import uvicorn
 import httpx
 
+from src.models import APIResponse
+
 app = FastAPI()
 
+PUNCTUATION = ('.', '!', '?')
+
+
+def check_punctuation(fact: str) -> bool:
+    return fact.endswith(PUNCTUATION)
+
+def check_length(fact: str, expected_length: int) -> bool:
+    return len(fact) == expected_length
+
 @app.get("/")
-async def read_root():
+async def main():
     async with httpx.AsyncClient() as client:
         resp = await client.get("https://catfact.ninja/fact")
-        print(resp)
         resp.raise_for_status()
-        return resp.json()
+        return APIResponse(
+            latency=0,
+            failed_request=True,
+            length_correct=True,
+            punctuation=True,
+        ).model_dump_json()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ src/models.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+class APIResponse(BaseModel):
+    latency: float
+    failed_request: bool
+    length_correct: bool
+    punctuation: bool
